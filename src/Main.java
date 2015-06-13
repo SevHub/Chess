9d6545d45cb25d77da51720ebca18c977a3bfcee
marius <marius.berger@t-online.de>
2015-06-13 15:02:40 +0200

    upload as directory

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+public class Main {
+	public static void main (String[] args) {
+		
+		GUI gui=createGUI();
+		
+	}
+	
+	
+	
+	public static GUI createGUI() {
+		Figure[] figureArr = new Figure[64];
+		return new GUI(figureArr);
+		
+		
+		
+		
+	}
+}
